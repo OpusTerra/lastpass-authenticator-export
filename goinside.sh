f0a7eed1b1c505595f018306aa6398d3feac32c4
Denis Lambert <opusterra.000@gmail.com>
2024-02-21 08:52:49 -0500

    Delete extra files

--- goinside.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Attach to container to run script
-echo
-echo
-echo Run './export.sh' when connected
-docker exec -it goodbye bash
--- makecontainer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# make a docker image
-docker build --tag byebyelp .
